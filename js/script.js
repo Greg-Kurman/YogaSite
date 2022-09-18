940e9c3602e214bfd23d11ac599e1929548e1184
greg <grigroiykurman@gmail.com>
2022-09-18 14:08:54 +0300

    slider on pure JS

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ window.addEventListener('load', function () {
 
     //Timer
 
-    let deadline = '2022-09-18'
+    let deadline = '2022-10-18'
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date())
@@ -83,10 +83,9 @@ window.addEventListener('load', function () {
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close'),
         description_btn = document.querySelectorAll('.description-btn');
-    console.log(description_btn[1])
 
-   
-   
+
+
 
 
     more.addEventListener('click', function () {
@@ -101,7 +100,6 @@ window.addEventListener('load', function () {
         document.body.style.overflow = ''
     })
 
-    console.log(description_btn.length)
 
     for (let i = 0; i < description_btn.length; i++) {
         description_btn[i].addEventListener('click', function () {
@@ -110,8 +108,119 @@ window.addEventListener('load', function () {
         })
     }
 
-    
+    //form 
+    let message = new Object();
+    message.loading = 'Загрузка...';
+    message.succes = 'Спасибо, скоро мы с вами свяжемся';
+    message.failure = 'Что-то пошло не так...';
 
 
+    let form = document.getElementsByClassName('main-form')[0],
+        input = document.getElementById('input'),
+        status_message = document.createElement('div');
+
+    status_message.classList.add('status');
+
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        form.appendChild(status_message)
+
+
+        //ajax
+
+        //  let request = new XMLHttpRequest()
+
+        //  request.open('POSRT', 'server.php')
+
+        //  request.setRequestHeader("Content-Type", "application/x-www-form-erlencoded");
+
+        //  let formData = new FormData(form)
+
+        //  request.send(formData)
+
+        //  request.onreadystatechange = function () {
+        //      if (request.readyState < 4) {
+        //          status_message.innerHTML = message.loading
+        //      } else if (request.readyState === 4) {
+        //          if (request.status == 200 && request.status < 300) {
+        //              status_message.innerHTML = message.succes
+        //              // добовляем контент на страницу
+        //          }
+
+        //          else {
+        //              status_message.innerHTML = message.failure
+        //          }
+        //      }
+        // }
+
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+            // Очищаем поля ввода
+        }
+
+
+
+
+
+    })
+
+
+    // slider
+
+    let slideIndex = 1,
+        sliders = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrapp = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSliders(slideIndex)
+
+    function showSliders(n) {
+        if (n > sliders.length) {
+            slideIndex = 1
+        }
+
+        if (n < 1) {
+            slideIndex = sliders.length
+        }
+
+        for (let i = 0; i < sliders.length; i++) {
+            sliders[i].style.display = 'none'
+        }
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove('dot-active')
+        }
+
+        sliders[slideIndex - 1].style.display = 'block'
+        dots[slideIndex - 1].classList.add('dot-active')
+    }
+
+    function plusSliders(n) {
+        showSliders(slideIndex += n)
+    }
+
+    function currentSlider(n) {
+        showSliders(slideIndex = n)
+    }
+
+    prev.addEventListener('click', function () {
+        plusSliders(-1)
+    })
+
+    next.addEventListener('click', function () {
+        plusSliders(1)
+    })
+
+    dotsWrapp.addEventListener('click', function (e) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+                currentSlider(i)
+            }
+        }
+    })
 
 })
